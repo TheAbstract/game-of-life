d05cda7c6a644aeadfc1d851f9c0b00397d3acdb
Siphiwe <siphiwe.m@protonmail.com>
2022-12-05 19:27:09 +0200

    @whereiwas: evolution of states

--- life.py
+++ life.py
@@ -10,14 +10,19 @@ def random_state(width=3, height=3):
     return board
 
 def display(board):
+    alive = '🙂 '
+    dead = '💀 '
     for row in board:
         for cell in row:
             print('* ' if cell == 1 else '. ', end='')
-    
+            # print(alive if cell == 1 else dead, end='')
         print('\n')
 
+def next_state(board):
+    pass
 
 if __name__ == '__main__':
+    # print(random_state(5, 5))
     board = random_state(2, 3)
     print(board)
     display(board)
